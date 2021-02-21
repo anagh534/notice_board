bdcef8c1a838501531407d0c19b3fba3fb644b15
ANAGH <anaghkrkkl@gmail.com>
2021-02-21 11:21:39 +0530

    notice board completed

--- server.js
+++ server.js
@@ -5,9 +5,22 @@ var io=require('socket.io')(http)
 
 app.get('/',(req,res)=>{
     var path=__dirname;
-    res.sendFile(path+"/public/board.html")
+    res.sendFile(path+"/public/board.html");
 })
 app.get('/admin',(req,res)=>{
     var path=__dirname;
-    res.sendFile(path+"/public/admin.html")
+    res.sendFile(path+"/public/admin.html");
+})
+io.on("connection",(socket)=>{
+    console.log("new connection established");
+    socket.on("disconnect",()=>{
+        console.log("connection closed");
+    })
+    socket.on('admin',(txt)=>{
+        console.log(txt);
+        io.emit('board_content',txt);
+    })
+})
+http.listen(3000,()=>{
+    console.log("Server Started");
 })
